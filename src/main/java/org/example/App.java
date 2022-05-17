d5a2462e03ad28ee7edf4ba90dee3a2f3769dc06
Adam Pusio <adam.pusio@outlook.com>
2022-05-17 18:23:38 +0200

    Hibernate save example

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,9 +1,15 @@
 package org.example;
 
+import org.example.model.Author;
 import org.example.model.Car;
+import org.example.model.Movie;
 import org.example.model.Wheel;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+import java.time.LocalDate;
+import java.util.List;
 
 public class App
 {
@@ -11,9 +17,25 @@ public class App
 //        Car fiatPanda = new Car("Fiat Panda", 2000, 3, 4);
 //        Car build = Car.builder().amountOfDoors(2).build();
 //        Wheel wheel = new Wheel();
+        Movie movie = new Movie();
+        movie.setTitle("Titanic");
+        movie.setReleaseDate(LocalDate.now());
+
+        Author author = new Author();
+        author.setFirstName("Adam");
+        author.setLastName("Adamski");
+        author.setAddress("Gdansk");
+
+        Author all = new Author(666L, "AAA", "BBB", "CCC");
 
         SessionFactory sessionFactory = new HibernateFactory().getSessionFactory();
         Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        //INSERT INTO MOVIE ...
+        session.save(movie);
+        session.save(author);
+        session.save(all);
+        transaction.commit();
         session.close();
         sessionFactory.close();
     }
--- src/main/java/org/example/model/Movie.java
+++ src/main/java/org/example/model/Movie.java
@@ -3,6 +3,7 @@ package org.example.model;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -14,6 +15,7 @@ import java.time.LocalDate;
 @Getter
 @Setter
 @NoArgsConstructor
+@ToString
 public class Movie {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
